4477d46dc3672ef4f452d264c7b93b42797dcde5
Mario00375 <49355365+Mario00375@users.noreply.github.com>
2019-10-29 13:02:25 -0600

    php agregar usuario

---
+++ Back/registrate.php
@@ -0,0 +1,15 @@
+<?PHP
+include 'conexion.php';
+
+$Nick = $_POST["nick"];
+$Nombre = $_POST["nombre"];
+$ApellidoP = $_POST["appellidoP"];
+$ApellidoM = $_POST["apellidoM"];
+$Password = $_POST["password"];
+
+$consulta = mysqli_query($conexion, "INSERT INTO Usuario VALUES('0', '$Nombre', '$ApellidoP', '$ApellidoM','$Nick', '$Password' )")
+or die("No se pudo subir");
+
+header('Location: /libreria/Front/login.html');
+
+?>
