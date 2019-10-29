b507a65c62524dfc25abfa042ae6e2257651cdc1
Mario00375 <49355365+Mario00375@users.noreply.github.com>
2019-10-29 13:44:38 -0600

    detalles

--- Back/comentar.php
+++ Back/comentar.php
@@ -2,8 +2,11 @@
 include 'conexion.php';
 
 $comentario = $_POST["coment"];
+$DateTime = date('Y-m-d');
 
-$consulta = mysqli_query($conexion, "INSERT INTO comentario VALUES('0', '$comentario', NULL, NULL, NULL)")
+$consulta = mysqli_query($conexion, "INSERT INTO comentario VALUES('0', '$comentario', '$DateTime', 1, 1)")
 or die("No se pudo subir");
+
+header('Location: /libreria/Front/listadoLibros.html');
 
 ?>
